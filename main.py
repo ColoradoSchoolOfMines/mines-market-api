b9826f16e619a908132829797ff4dda5081f7b58
Keenan D. Buckley <keenandbuckley@protonmail.com>
2023-09-28 13:33:34 -0600

    refactor getMenu and add function docs

--- main.py
+++ main.py
@@ -3,19 +3,34 @@ import requests
 import datetime
 
 
-# return info on mines
 def fetchLocationIds(location_name, apiKey):
+    """ search bite API for locations by location name
+
+    Args:
+        location_name (str): location to search for
+        apiKey (str): api key for the Sodexo's bite API
+
+    Returns:
+        str: json containing the http response
+    """
     url = "https://bite-external-api.azure-api.net/extern/accesscodeslocation"
     payload = {"name": location_name, "includeWithoutMenu": False}
     headers = {"Ocp-Apim-Subscription-Key": apiKey}
     return requests.get(url, params=payload, headers=headers).text
 
 
-# get menu
-def getMenu(apiKey):
+def fetchMenu(date, apiKey):
+    """ request from the bite API the menu for a specific date
+
+    Args:
+        date (datetime.date): date to fetch menu for
+        apiKey (str): api key for the Sodexo's bite API
+
+    Returns:
+        str: json containing the http response
+    """
     url = "https://bite-external-api.azure-api.net/extern/bite-application/location"
-    today = datetime.date.today()
-    params = {"date": today, "locationid": "75204001"}
+    params = {"date": date, "locationid": "75204001"}
     headers = {"Ocp-Apim-Subscription-Key": apiKey}
     return requests.get(url, params=params, headers=headers).text
 
@@ -29,4 +44,4 @@ with open("result.json", "w") as file:
     file.write(fetchLocationIds("mines", config["SODEXO_API_KEY"]))
 
 with open("info.json", "w", encoding="utf-8") as file:
-    file.write(getMenu(config["SODEXO_API_KEY"]))
+    file.write(fetchMenu(datetime.date.today(), config["SODEXO_API_KEY"]))

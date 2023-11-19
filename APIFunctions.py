49b7699497764168ab09ed2f49c3b944edd78a62
Yamato Matsumura <yamtheyam104@gmail.com>
2023-11-19 13:06:23 -0700

    Reorganized APIFunctions file

--- APIFunctions.py
+++ APIFunctions.py
@@ -3,20 +3,15 @@ import datetime
 
 
 # return info on mines
-def fetchLocationIds(location_name, apiKey):
-    url = "https://bite-external-api.azure-api.net/extern/accesscodeslocation"
+def fetchLocationIds(location_name, apiKey, url="https://bite-external-api.azure-api.net/extern/accesscodeslocation"):
     payload = {"name": location_name, "includeWithoutMenu": False}
     headers = {"Ocp-Apim-Subscription-Key": apiKey}
     return requests.get(url, params=payload, headers=headers).text
 
 
 # get menu
-def getMenu(apiKey, date=datetime.date.today()):
-    url = "https://bite-external-api.azure-api.net/extern/bite-application/location"
-    params = {"date": date, "locationid": "75204001"}
+def getMenu(apiKey, locationId, date=datetime.date.today(), url="https://bite-external-api.azure-api.net/extern/bite-application/location"):
+    params = {"date": date, "locationid": str(locationId)}
     headers = {"Ocp-Apim-Subscription-Key": apiKey}
     return requests.get(url, params=params, headers=headers).text
 
-
-def get_menu_info(menu_dict, x, key):
-    return menu_dict["Menus"][0]["OrderDays"][0]["MenuItems"][x]["BiteMenuItemSizes"][0][key]

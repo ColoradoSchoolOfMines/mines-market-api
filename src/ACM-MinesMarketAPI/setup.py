ee3f78b89491b1cddf10a202f3ff0b028511fa7f
Yamato Matsumura <yamtheyam104@gmail.com>
2023-11-26 20:04:26 -0700

    Renamed Package

---
+++ src/ACM-MinesMarketAPI/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ACM_MinesMarketAPI',
+    version='1.0.0',
+    author='Yamato Matsumura, Keenan Buckley',
+    author_email='matsumura.yamato@gmail.com, keenandbuckley@protonmail.com',
+    description='Access sodexo api to retrieve menu for desired location',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: 0BSD License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
